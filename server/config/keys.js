a2677dec26aad7fef7345f0270685fb696c9c4a7
duonghv <duonghv@bkav.com>
2018-12-11 00:53:08 +0700

    secure mongodb key

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,8 @@
-// PUT your mongoDB URI in here.
-module.exports = {
-  mongoURI:
-    'mongodb://duonghv:duonghv1101@ds227853.mlab.com:27853/survey-management',
-  secretOrKey: 'N0th1ng15S3cur3',
-};
+const keysProd = require('./keys_prod');
+const keysDev = require('./keys_dev');
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = keysProd;
+} else {
+  module.exports = keysDev;
+}
---
+++ server/config/keys_dev.js
@@ -0,0 +1,6 @@
+// PUT your mongoDB URI in here.
+module.exports = {
+  mongoURI:
+    'mongodb://duonghv:duonghv1101@ds227853.mlab.com:27853/survey-management',
+  secretOrKey: 'N0th1ng15S3cur3',
+};
